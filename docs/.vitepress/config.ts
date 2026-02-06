92d63bde089e8a43847294399587e5db9587a550
KuLiPai <yun.svip@gmail.com>
2026-02-06 14:27:55 +0800

    feat: 搜索功能

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,6 +8,32 @@ export default defineConfig({
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
 
+  themeConfig: {
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
 
   locales: {
     root: {
